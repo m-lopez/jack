f548af64e14e5f1b9e3030d000931c6fc4630110
Michael <michael.lopez.332@gmail.com>
2017-09-03 16:26:08 -0400

    Fixed compilation errors.

--- src/CodeGen/LLVM/Transformer.hs
+++ src/CodeGen/LLVM/Transformer.hs
@@ -10,23 +10,22 @@ Portability : non-portable
 module CodeGen.LLVM.Transformer ( toReadableLlvmIr ) where
 
 import CodeGen.LLVM.Ast ( toLlvmModuleAst )
+import Data.ByteString.Char8 ( unpack )
+import Expressions ( TlExpr )
+import LLVM.AST ( Module )
 import LLVM.Context ( withContext )
 import LLVM.Module ( moduleLLVMAssembly, withModuleFromAST )
-
-
+import Util.DebugOr ( DebugOr )
 
 -- | Transform an LLVM.AST.Module into human-readable LLVM IR.
-fromASTtoLlvmIr :: LLVM.AST.Module -> IO String
-fromASTtoLlvmIr m = BS.unpack <$> (withContext $ fromModAndCtx m)
+fromASTtoLlvmIr :: Module -> IO String
+fromASTtoLlvmIr m = unpack <$> (withContext $ fromModAndCtx m)
   where
     fromModAndCtx x y = withModuleFromAST y x moduleLLVMAssembly
 
 -- | Attempt to translate a Toaster module into human-readable LLVM IR.
 toReadableLlvmIr :: [TlExpr] -> DebugOr (IO String)
 toReadableLlvmIr prgm = do
-  ast <- toLlvmModuleAst
+  ast <- toLlvmModuleAst prgm
   return $ fromASTtoLlvmIr ast
 
-  pre <- toPreLLVM prgm
-  ast <- toLlvmModule pre
-  return $ fromASTtoLlvmIr ast
